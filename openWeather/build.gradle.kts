6afdf7953ed0e2b83d000354f118dca0f9e6f13a
Saeid Rezaei <saeed.rezaei.sa@gmail.com>
2023-12-19 23:34:08 +0100

    Add serialization and Weather data class

--- openWeather/build.gradle.kts
+++ openWeather/build.gradle.kts
@@ -26,6 +26,9 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(libs.ktor.client.core)
+            implementation(libs.ktor.serialization.json)
+            implementation(libs.ktor.client.cio)
+            implementation(libs.ktor.client.content.negotiation)
         }
         iosMain.dependencies {
             implementation(libs.ktor.client.darwin)
--- openWeather/src/commonMain/kotlin/saeedrz/yawa/openweather/OpenWeatherAPI.kt
+++ openWeather/src/commonMain/kotlin/saeedrz/yawa/openweather/OpenWeatherAPI.kt
@@ -1,7 +1,16 @@
 package saeedrz.yawa.openweather
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.*
+
 class OpenWeatherAPI {
-    private val platform: Platform = getPlatform()
+    private val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
 
     fun getCurrentWeatherData(): String {
         return "Hello Weather"
