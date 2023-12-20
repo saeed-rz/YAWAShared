48a466ddf798da26d3c22d93b3cece06c09d4485
Saeid Rezaei <saeed.rezaei.sa@gmail.com>
2023-12-20 15:36:18 +0100

    Implement api

--- openWeather/src/commonMain/kotlin/saeedrz/yawa/openweather/OpenWeatherAPI.kt
+++ openWeather/src/commonMain/kotlin/saeedrz/yawa/openweather/OpenWeatherAPI.kt
@@ -1,9 +1,12 @@
 package saeedrz.yawa.openweather
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
 import io.ktor.serialization.kotlinx.json.*
+import saeedrz.yawa.openweather.data.WeatherData
 
 class OpenWeatherAPI {
     private val client = HttpClient(CIO) {
@@ -12,7 +15,7 @@ class OpenWeatherAPI {
         }
     }
 
-    fun getCurrentWeatherData(): String {
-        return "Hello Weather"
+    suspend fun getCurrentWeatherData(lat: Double, lon: Double, appId: String): WeatherData {
+        return client.get("https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$appId").body<WeatherData>()
     }
 }
--- release/YAWAShared.xcframework/ios-arm64_x86_64-simulator/YAWAShared.framework/Headers/YAWAShared.h
+++ release/YAWAShared.xcframework/ios-arm64_x86_64-simulator/YAWAShared.framework/Headers/YAWAShared.h
@@ -6,7 +6,9 @@
 #import <Foundation/NSString.h>
 #import <Foundation/NSValue.h>
 
-@protocol YAWASPlatform, YAWASPlatform_;
+@class YAWASWeatherData, YAWASClouds, YAWASCoord, YAWASMain, YAWASSys, YAWASWeather, YAWASWind, YAWASKotlinThrowable, YAWASKotlinArray<T>, YAWASKotlinException, YAWASKotlinRuntimeException, YAWASKotlinIllegalStateException;
+
+@protocol YAWASPlatform, YAWASPlatform_, YAWASKotlinIterator;
 
 NS_ASSUME_NONNULL_BEGIN
 #pragma clang diagnostic push
@@ -183,7 +185,151 @@ __attribute__((swift_name("OpenWeatherAPI")))
 @interface YAWASOpenWeatherAPI : YAWASBase
 - (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
 + (instancetype)new __attribute__((availability(swift, unavailable, message="use object initializers instead")));
-- (NSString *)getCurrentWeatherData __attribute__((swift_name("getCurrentWeatherData()")));
+
+/**
+ * @note This method converts instances of CancellationException to errors.
+ * Other uncaught Kotlin exceptions are fatal.
+*/
+- (void)getCurrentWeatherDataLat:(double)lat lon:(double)lon appId:(NSString *)appId completionHandler:(void (^)(YAWASWeatherData * _Nullable, NSError * _Nullable))completionHandler __attribute__((swift_name("getCurrentWeatherData(lat:lon:appId:completionHandler:)")));
+@end
+
+
+/**
+ * @note annotations
+ *   kotlinx.serialization.Serializable
+*/
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("Clouds")))
+@interface YAWASClouds : YAWASBase
+- (instancetype)initWithAll:(int64_t)all __attribute__((swift_name("init(all:)"))) __attribute__((objc_designated_initializer));
+- (YAWASClouds *)doCopyAll:(int64_t)all __attribute__((swift_name("doCopy(all:)")));
+- (BOOL)isEqual:(id _Nullable)other __attribute__((swift_name("isEqual(_:)")));
+- (NSUInteger)hash __attribute__((swift_name("hash()")));
+- (NSString *)description __attribute__((swift_name("description()")));
+@property (readonly) int64_t all __attribute__((swift_name("all")));
+@end
+
+
+/**
+ * @note annotations
+ *   kotlinx.serialization.Serializable
+*/
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("Coord")))
+@interface YAWASCoord : YAWASBase
+- (instancetype)initWithLon:(double)lon lat:(double)lat __attribute__((swift_name("init(lon:lat:)"))) __attribute__((objc_designated_initializer));
+- (YAWASCoord *)doCopyLon:(double)lon lat:(double)lat __attribute__((swift_name("doCopy(lon:lat:)")));
+- (BOOL)isEqual:(id _Nullable)other __attribute__((swift_name("isEqual(_:)")));
+- (NSUInteger)hash __attribute__((swift_name("hash()")));
+- (NSString *)description __attribute__((swift_name("description()")));
+@property (readonly) double lat __attribute__((swift_name("lat")));
+@property (readonly) double lon __attribute__((swift_name("lon")));
+@end
+
+
+/**
+ * @note annotations
+ *   kotlinx.serialization.Serializable
+*/
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("Main")))
+@interface YAWASMain : YAWASBase
+- (instancetype)initWithTemp:(double)temp feelsLike:(double)feelsLike tempMin:(double)tempMin tempMax:(double)tempMax pressure:(int64_t)pressure humidity:(int64_t)humidity __attribute__((swift_name("init(temp:feelsLike:tempMin:tempMax:pressure:humidity:)"))) __attribute__((objc_designated_initializer));
+- (YAWASMain *)doCopyTemp:(double)temp feelsLike:(double)feelsLike tempMin:(double)tempMin tempMax:(double)tempMax pressure:(int64_t)pressure humidity:(int64_t)humidity __attribute__((swift_name("doCopy(temp:feelsLike:tempMin:tempMax:pressure:humidity:)")));
+- (BOOL)isEqual:(id _Nullable)other __attribute__((swift_name("isEqual(_:)")));
+- (NSUInteger)hash __attribute__((swift_name("hash()")));
+- (NSString *)description __attribute__((swift_name("description()")));
+@property (readonly) double feelsLike __attribute__((swift_name("feelsLike")));
+@property (readonly) int64_t humidity __attribute__((swift_name("humidity")));
+@property (readonly) int64_t pressure __attribute__((swift_name("pressure")));
+@property (readonly) double temp __attribute__((swift_name("temp")));
+@property (readonly) double tempMax __attribute__((swift_name("tempMax")));
+@property (readonly) double tempMin __attribute__((swift_name("tempMin")));
+@end
+
+
+/**
+ * @note annotations
+ *   kotlinx.serialization.Serializable
+*/
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("Sys")))
+@interface YAWASSys : YAWASBase
+- (instancetype)initWithType:(int64_t)type id:(int64_t)id country:(NSString *)country sunrise:(int64_t)sunrise sunset:(int64_t)sunset __attribute__((swift_name("init(type:id:country:sunrise:sunset:)"))) __attribute__((objc_designated_initializer));
+- (YAWASSys *)doCopyType:(int64_t)type id:(int64_t)id country:(NSString *)country sunrise:(int64_t)sunrise sunset:(int64_t)sunset __attribute__((swift_name("doCopy(type:id:country:sunrise:sunset:)")));
+- (BOOL)isEqual:(id _Nullable)other __attribute__((swift_name("isEqual(_:)")));
+- (NSUInteger)hash __attribute__((swift_name("hash()")));
+- (NSString *)description __attribute__((swift_name("description()")));
+@property (readonly) NSString *country __attribute__((swift_name("country")));
+@property (readonly) int64_t id __attribute__((swift_name("id")));
+@property (readonly) int64_t sunrise __attribute__((swift_name("sunrise")));
+@property (readonly) int64_t sunset __attribute__((swift_name("sunset")));
+@property (readonly) int64_t type __attribute__((swift_name("type")));
+@end
+
+
+/**
+ * @note annotations
+ *   kotlinx.serialization.Serializable
+*/
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("Weather")))
+@interface YAWASWeather : YAWASBase
+- (instancetype)initWithId:(int64_t)id main:(NSString *)main description:(NSString *)description icon:(NSString *)icon __attribute__((swift_name("init(id:main:description:icon:)"))) __attribute__((objc_designated_initializer));
+- (YAWASWeather *)doCopyId:(int64_t)id main:(NSString *)main description:(NSString *)description icon:(NSString *)icon __attribute__((swift_name("doCopy(id:main:description:icon:)")));
+- (BOOL)isEqual:(id _Nullable)other __attribute__((swift_name("isEqual(_:)")));
+- (NSUInteger)hash __attribute__((swift_name("hash()")));
+- (NSString *)description __attribute__((swift_name("description()")));
+@property (readonly) NSString *description_ __attribute__((swift_name("description_")));
+@property (readonly) NSString *icon __attribute__((swift_name("icon")));
+@property (readonly) int64_t id __attribute__((swift_name("id")));
+@property (readonly) NSString *main __attribute__((swift_name("main")));
+@end
+
+
+/**
+ * @note annotations
+ *   kotlinx.serialization.Serializable
+*/
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("WeatherData")))
+@interface YAWASWeatherData : YAWASBase
+- (instancetype)initWithCoord:(YAWASCoord *)coord weather:(NSArray<YAWASWeather *> *)weather base:(NSString *)base main:(YAWASMain *)main visibility:(int64_t)visibility wind:(YAWASWind *)wind clouds:(YAWASClouds *)clouds dt:(int64_t)dt sys:(YAWASSys *)sys timezone:(int64_t)timezone id:(int64_t)id name:(NSString *)name cod:(int64_t)cod __attribute__((swift_name("init(coord:weather:base:main:visibility:wind:clouds:dt:sys:timezone:id:name:cod:)"))) __attribute__((objc_designated_initializer));
+- (YAWASWeatherData *)doCopyCoord:(YAWASCoord *)coord weather:(NSArray<YAWASWeather *> *)weather base:(NSString *)base main:(YAWASMain *)main visibility:(int64_t)visibility wind:(YAWASWind *)wind clouds:(YAWASClouds *)clouds dt:(int64_t)dt sys:(YAWASSys *)sys timezone:(int64_t)timezone id:(int64_t)id name:(NSString *)name cod:(int64_t)cod __attribute__((swift_name("doCopy(coord:weather:base:main:visibility:wind:clouds:dt:sys:timezone:id:name:cod:)")));
+- (BOOL)isEqual:(id _Nullable)other __attribute__((swift_name("isEqual(_:)")));
+- (NSUInteger)hash __attribute__((swift_name("hash()")));
+- (NSString *)description __attribute__((swift_name("description()")));
+@property (readonly) NSString *base __attribute__((swift_name("base")));
+@property (readonly) YAWASClouds *clouds __attribute__((swift_name("clouds")));
+@property (readonly) int64_t cod __attribute__((swift_name("cod")));
+@property (readonly) YAWASCoord *coord __attribute__((swift_name("coord")));
+@property (readonly) int64_t dt __attribute__((swift_name("dt")));
+@property (readonly) int64_t id __attribute__((swift_name("id")));
+@property (readonly) YAWASMain *main __attribute__((swift_name("main")));
+@property (readonly) NSString *name __attribute__((swift_name("name")));
+@property (readonly) YAWASSys *sys __attribute__((swift_name("sys")));
+@property (readonly) int64_t timezone __attribute__((swift_name("timezone")));
+@property (readonly) int64_t visibility __attribute__((swift_name("visibility")));
+@property (readonly) NSArray<YAWASWeather *> *weather __attribute__((swift_name("weather")));
+@property (readonly) YAWASWind *wind __attribute__((swift_name("wind")));
+@end
+
+
+/**
+ * @note annotations
+ *   kotlinx.serialization.Serializable
+*/
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("Wind")))
+@interface YAWASWind : YAWASBase
+- (instancetype)initWithSpeed:(double)speed deg:(int64_t)deg gust:(double)gust __attribute__((swift_name("init(speed:deg:gust:)"))) __attribute__((objc_designated_initializer));
+- (YAWASWind *)doCopySpeed:(double)speed deg:(int64_t)deg gust:(double)gust __attribute__((swift_name("doCopy(speed:deg:gust:)")));
+- (BOOL)isEqual:(id _Nullable)other __attribute__((swift_name("isEqual(_:)")));
+- (NSUInteger)hash __attribute__((swift_name("hash()")));
+- (NSString *)description __attribute__((swift_name("description()")));
+@property (readonly) int64_t deg __attribute__((swift_name("deg")));
+@property (readonly) double gust __attribute__((swift_name("gust")));
+@property (readonly) double speed __attribute__((swift_name("speed")));
 @end
 
 __attribute__((objc_subclassing_restricted))
@@ -196,6 +342,86 @@ __attribute__((objc_subclassing_restricted))
 __attribute__((swift_name("Platform_iosKt_")))
 @interface YAWASPlatform_iosKt_ : YAWASBase
 + (id<YAWASPlatform_>)getPlatform __attribute__((swift_name("getPlatform()")));
+@end
+
+__attribute__((swift_name("KotlinThrowable")))
+@interface YAWASKotlinThrowable : YAWASBase
+- (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
++ (instancetype)new __attribute__((availability(swift, unavailable, message="use object initializers instead")));
+- (instancetype)initWithMessage:(NSString * _Nullable)message __attribute__((swift_name("init(message:)"))) __attribute__((objc_designated_initializer));
+- (instancetype)initWithCause:(YAWASKotlinThrowable * _Nullable)cause __attribute__((swift_name("init(cause:)"))) __attribute__((objc_designated_initializer));
+- (instancetype)initWithMessage:(NSString * _Nullable)message cause:(YAWASKotlinThrowable * _Nullable)cause __attribute__((swift_name("init(message:cause:)"))) __attribute__((objc_designated_initializer));
+
+/**
+ * @note annotations
+ *   kotlin.experimental.ExperimentalNativeApi
+*/
+- (YAWASKotlinArray<NSString *> *)getStackTrace __attribute__((swift_name("getStackTrace()")));
+- (void)printStackTrace __attribute__((swift_name("printStackTrace()")));
+- (NSString *)description __attribute__((swift_name("description()")));
+@property (readonly) YAWASKotlinThrowable * _Nullable cause __attribute__((swift_name("cause")));
+@property (readonly) NSString * _Nullable message __attribute__((swift_name("message")));
+- (NSError *)asError __attribute__((swift_name("asError()")));
+@end
+
+__attribute__((swift_name("KotlinException")))
+@interface YAWASKotlinException : YAWASKotlinThrowable
+- (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
++ (instancetype)new __attribute__((availability(swift, unavailable, message="use object initializers instead")));
+- (instancetype)initWithMessage:(NSString * _Nullable)message __attribute__((swift_name("init(message:)"))) __attribute__((objc_designated_initializer));
+- (instancetype)initWithCause:(YAWASKotlinThrowable * _Nullable)cause __attribute__((swift_name("init(cause:)"))) __attribute__((objc_designated_initializer));
+- (instancetype)initWithMessage:(NSString * _Nullable)message cause:(YAWASKotlinThrowable * _Nullable)cause __attribute__((swift_name("init(message:cause:)"))) __attribute__((objc_designated_initializer));
+@end
+
+__attribute__((swift_name("KotlinRuntimeException")))
+@interface YAWASKotlinRuntimeException : YAWASKotlinException
+- (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
++ (instancetype)new __attribute__((availability(swift, unavailable, message="use object initializers instead")));
+- (instancetype)initWithMessage:(NSString * _Nullable)message __attribute__((swift_name("init(message:)"))) __attribute__((objc_designated_initializer));
+- (instancetype)initWithCause:(YAWASKotlinThrowable * _Nullable)cause __attribute__((swift_name("init(cause:)"))) __attribute__((objc_designated_initializer));
+- (instancetype)initWithMessage:(NSString * _Nullable)message cause:(YAWASKotlinThrowable * _Nullable)cause __attribute__((swift_name("init(message:cause:)"))) __attribute__((objc_designated_initializer));
+@end
+
+__attribute__((swift_name("KotlinIllegalStateException")))
+@interface YAWASKotlinIllegalStateException : YAWASKotlinRuntimeException
+- (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
++ (instancetype)new __attribute__((availability(swift, unavailable, message="use object initializers instead")));
+- (instancetype)initWithMessage:(NSString * _Nullable)message __attribute__((swift_name("init(message:)"))) __attribute__((objc_designated_initializer));
+- (instancetype)initWithCause:(YAWASKotlinThrowable * _Nullable)cause __attribute__((swift_name("init(cause:)"))) __attribute__((objc_designated_initializer));
+- (instancetype)initWithMessage:(NSString * _Nullable)message cause:(YAWASKotlinThrowable * _Nullable)cause __attribute__((swift_name("init(message:cause:)"))) __attribute__((objc_designated_initializer));
+@end
+
+
+/**
+ * @note annotations
+ *   kotlin.SinceKotlin(version="1.4")
+*/
+__attribute__((swift_name("KotlinCancellationException")))
+@interface YAWASKotlinCancellationException : YAWASKotlinIllegalStateException
+- (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
++ (instancetype)new __attribute__((availability(swift, unavailable, message="use object initializers instead")));
+- (instancetype)initWithMessage:(NSString * _Nullable)message __attribute__((swift_name("init(message:)"))) __attribute__((objc_designated_initializer));
+- (instancetype)initWithCause:(YAWASKotlinThrowable * _Nullable)cause __attribute__((swift_name("init(cause:)"))) __attribute__((objc_designated_initializer));
+- (instancetype)initWithMessage:(NSString * _Nullable)message cause:(YAWASKotlinThrowable * _Nullable)cause __attribute__((swift_name("init(message:cause:)"))) __attribute__((objc_designated_initializer));
+@end
+
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("KotlinArray")))
+@interface YAWASKotlinArray<T> : YAWASBase
++ (instancetype)arrayWithSize:(int32_t)size init:(T _Nullable (^)(YAWASInt *))init __attribute__((swift_name("init(size:init:)")));
++ (instancetype)alloc __attribute__((unavailable));
++ (instancetype)allocWithZone:(struct _NSZone *)zone __attribute__((unavailable));
+- (T _Nullable)getIndex:(int32_t)index __attribute__((swift_name("get(index:)")));
+- (id<YAWASKotlinIterator>)iterator __attribute__((swift_name("iterator()")));
+- (void)setIndex:(int32_t)index value:(T _Nullable)value __attribute__((swift_name("set(index:value:)")));
+@property (readonly) int32_t size __attribute__((swift_name("size")));
+@end
+
+__attribute__((swift_name("KotlinIterator")))
+@protocol YAWASKotlinIterator
+@required
+- (BOOL)hasNext __attribute__((swift_name("hasNext()")));
+- (id _Nullable)next __attribute__((swift_name("next()")));
 @end
 
 #pragma pop_macro("_Nullable_result")
