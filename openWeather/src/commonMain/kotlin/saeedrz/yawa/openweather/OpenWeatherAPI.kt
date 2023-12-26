fd976decfa516c32f1ec6ad9b9bc5655fb4a9970
Saeid Rezaei <saeed.rezaei.sa@gmail.com>
2023-12-26 20:06:54 +0100

    Geo coding implemented

--- openWeather/src/commonMain/kotlin/saeedrz/yawa/openweather/OpenWeatherAPI.kt
+++ openWeather/src/commonMain/kotlin/saeedrz/yawa/openweather/OpenWeatherAPI.kt
@@ -6,6 +6,7 @@ import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.serialization.kotlinx.json.*
+import saeedrz.yawa.openweather.data.GeoData
 import saeedrz.yawa.openweather.data.WeatherData
 
 class OpenWeatherAPI {
@@ -17,5 +18,9 @@ class OpenWeatherAPI {
 
     suspend fun getCurrentWeatherData(lat: Double, lon: Double, appId: String): WeatherData {
         return client.get("https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$appId").body<WeatherData>()
+    }
+
+    suspend fun searchCity(searchTerm: String, appId: String): List<GeoData> {
+        return client.get("http://api.openweathermap.org/geo/1.0/direct?q=$searchTerm&limit=5&appid=$appId").body<List<GeoData>>()
     }
 }
